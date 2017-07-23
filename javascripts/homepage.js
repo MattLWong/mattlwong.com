05e34510367bad90fce4efa35d3293d8c0e242b2
Matt <matthewlockewong@gmail.com>
2017-07-22 23:32:45 -0700

    adjusted css and fixed safari height bug

--- javascripts/homepage.js
+++ javascripts/homepage.js
@@ -1,10 +1,10 @@
 jQuery(function($) {
     $('.slider').bxSlider({
         adaptiveHeight: true,
-        mode: 'fade',
-        auto: true
+        // mode: 'fade'
+        // auto: true
     });
-    $('.slider-home').bxSlider({
+    $('.slider-auto').bxSlider({
         adaptiveHeight: true,
         mode: 'fade',
         auto: true
